bf01445dfd67051e9a584920e351d6a670065113
efeoran <efeoran1@outlook.com>
2023-06-10 22:30:57 +0300

    5- Karakter kontrolleri ve kamera eklendi.
    Karakterin yürüme ve zıplama hareketleri kodlandı. Cinemachine eklenerek kamera takibi ve karakteri görüş açısı ayarlandı. Zıplama sadece "Platform" tagi eklenmiş nesnelerin üzerinde çalışacak şekilde ayarlandı.

---
+++ Assets/Scripts/CameraFollowScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowScript : MonoBehaviour
+{
+    
+    
+    //public Transform Player; // Oyuncu karakterinin Transform bileþeni
+    ////public float rotationSpeed = 0f; // Dönme hýzý
+
+    //private Vector3 initialRotation; // Baþlangýçta karakterin dönme açýsý
+    //private Vector3 currentRotation; // Güncel karakterin dönme açýsý
+
+    //private void Start()
+    //{
+    //    //initialRotation = Player.eulerAngles;
+    //    //currentRotation = initialRotation;
+    //    //Cursor.lockState = CursorLockMode.Locked; // Mouse'u ekranýn ortasýnda sabitle
+        
+    //}
+
+}
