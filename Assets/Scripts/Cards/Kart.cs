e035f641a7f8cf3d44ac037f7148e1c4ccd246f2
ozlem1dev <ozlem.dev@hotmail.com>
2023-07-02 23:04:39 +0300

    111-Kart mekanikleri ve yeni tasarlanan ana sahne eklendi
    Kart scriptleri, assetleri eklendi. Örnek sahne eklendi. Coin tasarımı yapıldı ve o da dosyalara eklendi. Yeni tasarlanan sahne projeye eklendi.

---
+++ Assets/Scripts/Cards/Kart.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kart
+{
+    public string ad { get; set; } = string.Empty;
+    public string aciklama { get; set; } = string.Empty;
+    public bool aktiflik { get; set; } = true;
+    public int kalanAdet { get; set; } = 0;
+    public int indeks { get; set; } = 0;
+    public float olasilik { get; set; }
+    public int minLevel { get; set; }
+    public int maxLevel { get; set; }
+    public int lvl { get; set; }
+    public int cost { get; set; }
+
+    public Sprite gorsel { get; set; }
+
+    public delegate void KartSecildigindeHandler();
+    public event KartSecildigindeHandler KartSecildiginde;
+    private static int kartSayac = 0;
+    public Kart()
+    {
+        indeks = kartSayac;
+        kartSayac++;
+    }
+    public void OnKartSecildiginde()
+    {
+        if (KartSecildiginde != null)
+        {
+            KartSecildiginde.Invoke();
+        }
+    }
+}
+
